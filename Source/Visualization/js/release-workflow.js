d7835176982d3b809c6e4b8dea5c77bc2906024b
Eric Burcham <eburcham@eprod.com>
2017-03-02 20:44:04 -0600

    Finished up the release workflow.

--- Source/Visualization/js/release-workflow.js
+++ Source/Visualization/js/release-workflow.js
@@ -10,31 +10,32 @@ var gitgraph = new GitGraph({
 var master = gitgraph.branch("master");
 master.checkout();
 master.commit({
-    sha1: "803bd7",
     message: "Initial Commit",
-    author: "Eric Burcham <eburcham@eprod.com>",
+    author: "Eric Burcham <eburcham@eprod.com>"
 });
 
 var develop = master.branch("develop");
 develop.checkout();
 develop.commit({
-    sha1: "783d17",
     message: "Created develop branch.",
-    author: "Eric Burcham <eburcham@eprod.com>",
+    author: "Eric Burcham <eburcham@eprod.com>"
 });
 
-var addFeatureWorkflow = develop.branch("f-add-feature-workflow");
-addFeatureWorkflow.commit({
-    sha1: "bfb874",
-    message: "Master branch created with initial commit.  Develop branch created.  Time to put some text next to them.",
-    author: "Eric Burcham <eburcham@eprod.com>",
+var release = develop.branch("f-add-feature-workflow");
+release.checkout();
+release.commit({
+    message: "Release branch created.",
+    author: "Eric Burcham <eburcham@eprod.com>"
 });
 
-addFeatureWorkflow.commit({
-    sha1: "6dcfb7",
-    message: "Got the visualization placed next to the text.",
-    author: "Eric Burcham <eburcham@eprod.com>",
+release.commit({
+    message: "Fixed the last bug.",
+    author: "Eric Burcham <eburcham@eprod.com>"
 });
 
-addFeatureWorkflow.merge(develop, { dotColor: "red" });
+release.merge(master);
+master.tag("v1.0.0.0");
 
+release.merge(develop);
+release.delete();
+master.checkout();
