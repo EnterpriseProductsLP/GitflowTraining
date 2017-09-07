167d979cdd3671f6fb8aefccb2e81e251870e085
Eric Burcham <jameseburcham@gmail.com>
2017-09-06 20:14:52 -0500

    Updated flow.

--- Source/Visualization/js/support-overview.js
+++ Source/Visualization/js/support-overview.js
@@ -8,9 +8,34 @@ var gitgraph = new GitGraph({
     elementId: "gitGraph"
 });
 
+// Create master and develop branches.
 var master = gitgraph.branch("master").commit().commit();
 var develop = gitgraph.branch("develop").commit();
+
+// Add some initial commits for good measure.
 master.commit();
 develop.commit().commit();
 master.commit();
-develop.merge(master);
+
+// Create a release branch from master.
+var release = master.branch("release");
+
+// Create a feature from master.
+var feature = master.branch("feature").commit().commit();
+feature.merge(release);
+
+var hotfix = master.branch("hotfix").commit().commit().commit();
+hotfix.merge(develop);
+hotfix.merge(master);
+
+var bugfix = master.branch("bugfix").commit();
+bugfix.merge(release);
+
+// Finish the release.s
+release.merge(master);
+
+// Merge to develop and clean up.
+feature.merge(develop);
+feature.delete();
+bugfix.merge(develop);
+bugfix.delete();
